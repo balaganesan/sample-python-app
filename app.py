7fd706dabd5c311bf522bfe078c93449523e0493
balaganesan <ganesan.bala@gmail.com>
2025-02-05 22:29:13 +0000

    updated to 0.5

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world - UPDATED THE CODE VERSION - 0.4!'
+    return 'Hello, world - UPDATED THE CODE VERSION - 0.5!'
 
 if __name__ == '__main__':
     app.run()
