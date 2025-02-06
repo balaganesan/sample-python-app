2113e9d10dc7ce99078db022890c4fee6f1473b2
balaganesan <ganesan.bala@gmail.com>
2025-02-06 18:09:03 +0000

    updated the version4

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 helloworld = Flask(__name__)
 @helloworld.route("/")
 def run():
-    return "{\"message\":\"Hello World Python V3 3333 \"}"
+    return "{\"message\":\"Hello World Python V4 4444 \"}"
 if __name__ == "__main__":
     helloworld.run(host="0.0.0.0", port=int("80"), debug=True)
 
