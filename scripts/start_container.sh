2a018ede26c7578cfbedb66106e87987521afc80
balaganesan <ganesan.bala@gmail.com>
2025-02-06 13:19:06 +0000

    updated to port 80

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull balag12345/sample-python-app:RELEASE-0.1
 
 # Run the Docker image as a container
-docker run -d -p 5001:5000 balag12345/sample-python-app:RELEASE-0.1
+docker run -d -p 80:80 balag12345/sample-python-app:RELEASE-0.1
