745d95fe2bb14aa60ad4049644965d57f750f1c0
balaganesan <ganesan.bala@gmail.com>
2025-02-06 13:24:37 +0000

    updated app.py

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return 'Hello, world - UPDATED THE CODE VERSION - 0.7!'
-
-if __name__ == '__main__':
-    app.run()
+helloworld = Flask(__name__)
+@helloworld.route("/")
+def run():
+    return "{\"message\":\"Hello World Python v2\"}"
+if __name__ == "__main__":
+    helloworld.run(host="0.0.0.0", port=int("80"), debug=True)
 
